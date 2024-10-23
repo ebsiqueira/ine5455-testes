8c970e5664f1e06c6a8115a53615eb4272295107
Eduardo Siqueira <eduardosiqueira26@gmail.com>
2024-10-23 09:50:41 -0300

    Teste empresa sem nome

--- exercicio_8/test_company.py
+++ exercicio_8/test_company.py
@@ -8,6 +8,7 @@
 # Dois funcionarios em um projeto
 
 # Nao felizes:
+# Criar empresa sem nome
 # Criar projeto sem empresa
 # Criar funcionario sem empresa
 # Criar dois funcionarios iguais
@@ -22,6 +23,10 @@ class TestBoard(unittest.TestCase):
     def test_criar_empresa(self):
         empresa = Empresa('Figueirense S.A')
         self.assertEqual(empresa.nome, 'Figueirense S.A')
-
+        
+    def test_criar_empresa_sem_nome(self):
+        with self.assertRaises(Exception):
+            empresa = Empresa()
+        
 if __name__ == '__main__':
     unittest.main()
