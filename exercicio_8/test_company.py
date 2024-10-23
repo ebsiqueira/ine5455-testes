0d2a0722a95dc210506410227e00c953a386181f
Eduardo Siqueira <eduardosiqueira26@gmail.com>
2024-10-23 09:35:24 -0300

    Definicao dos testes

---
+++ exercicio_8/test_company.py
@@ -0,0 +1,15 @@
+# Criar empresa
+# Criar funcionario
+# Criar projeto
+# Criar um funcionario na empresa
+# Criar projeto na empresa
+# Um funcionario em um projeto
+# Um funcionaro em dois projetos
+# Dois funcionarios em um projeto
+
+# Nao felizes:
+# Criar projeto sem empresa
+# Criar funcionario sem empresa
+# Criar dois funcionarios iguais
+
+# Criar funcionario em uma empresa e cadastrar ele em um projeto de outra empresa
